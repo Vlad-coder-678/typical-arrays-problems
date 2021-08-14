d831344272a8a9a6e4b7527103ed61fa70b85b1a
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-08-14 20:36:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    }
+    return array.sort((a, b) => a - b)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    }
+    return array.sort((a, b) => b - a)[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    }
+    return (
+        Math.floor(
+            (array.reduce((acc, i) => acc + i, 0) / array.length) * 100
+        ) / 100
+    );
+};
